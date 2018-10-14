3809548921bf626d9b5eaa918798df071c51a461
HenryZheng <h79121@gmail.com>
2018-10-14 14:25:03 +0800

    remove spring security

--- src/main/java/com/fatcup/backend/FatcupApplication.java
+++ src/main/java/com/fatcup/backend/FatcupApplication.java
@@ -1,15 +1,13 @@
 package com.fatcup.backend;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.core.io.ClassPathResource;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -23,30 +21,15 @@ public class FatcupApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FatcupApplication.class, args);
 	}
-	
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true); // you USUALLY want this
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("POST");
-		config.addAllowedHeader("x-firebase-auth");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
 
 	@Bean
 	public FirebaseAuth firebaseAuth() throws IOException {
 
-		FileInputStream serviceAccount = new FileInputStream("firebase-adminsdk.json");
+		InputStream serviceAccount = new ClassPathResource("firebase-adminsdk.json").getInputStream();
 
 		FirebaseOptions options = new FirebaseOptions.Builder()
 				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.setDatabaseUrl("https://mydatabaseurl.firebaseio.com/").build();
+				.setDatabaseUrl("https://fatcup-216514.firebaseio.com").build();
 
 		FirebaseApp.initializeApp(options);
 
--- src/main/java/com/fatcup/backend/UserController.java
+++ src/main/java/com/fatcup/backend/UserController.java
@@ -1,5 +1,8 @@
 package com.fatcup.backend;
 
+import java.io.Console;
+
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +12,21 @@ import com.fatcup.backend.response.register;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+	
 	@RequestMapping(value = "/register")
 	public @ResponseBody register register(){
 		return new register("200");
+	}
+	
+	@RequestMapping(value = "/check")
+	public @ResponseBody register check(@RequestHeader(value="Authorization") String token) {
+		
+		System.out.println(token);
+		return new register(token);
+	}
+	
+	@RequestMapping(value = "/check2")
+	public void check2() {
+		
 	}
 }
